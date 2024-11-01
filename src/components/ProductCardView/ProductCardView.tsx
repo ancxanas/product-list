c45d93ff4d0d0e823d77668957daec4cce268237
Muhammed Anas <anas.corona.github@gmail.com>
2024-11-01 09:23:11 +0530

    Implemented ProductCardViewComponent

---
+++ src/components/ProductCardView/ProductCardView.tsx
@@ -0,0 +1,19 @@
+import ProductCard from "../ProductCard/ProductCard";
+import { Product } from "../../types/product";
+import "./ProductCardView.css";
+
+interface ProductCardViewProps {
+  products: Product[];
+}
+
+const ProductCardView: React.FC<ProductCardViewProps> = ({ products }) => {
+  return (
+    <div className="product-card-view-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductCardView;
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { Product } from "../../types/product";
+import { View } from "../../types/view";
 import productService from "../../services/product";
-import ProductCard from "../ProductCard/ProductCard";
 import "./ProductList.css";
+import ProductCardView from "../ProductCardView/ProductCardView";
 
 const ProductList = () => {
+  const [view, setView] = useState<View>("card");
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
@@ -16,10 +18,14 @@ const ProductList = () => {
   return (
     <div className="product-list-container">
       <h1 className="product-list-title">Product List</h1>
+      <button className="table-view-button" onClick={() => setView("table")}>
+        Table
+      </button>
+      <button className="card-view-button" onClick={() => setView("card")}>
+        Card
+      </button>
       <div className="product-list">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+        {view === "card" ? <ProductCardView products={products} /> : "hello"}
       </div>
     </div>
   );
